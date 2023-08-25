b972fe61c1c2ec5e25a7ddd93102d2b2b4158c16
Simon Gschnell <simongschnell@gmail.com>
2023-08-25 15:35:15 +0200

    creates chess pieces from a list, listen to emits and passes props

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <script setup>
 import Tile from './components/Tile.vue';
 
+const pieces=[{symbol:"♔",id:"a1"}, {symbol:"♗",id:"a2"}, {symbol:"♟︎",id:"a3"},]
+function fromFun(id){
+console.log(id)
+}
+function toFun(id){
+    console.log(id)
+
+
+}
 </script>
 
 <template>  
      
-<Tile/>
+<Tile v-for="ele in pieces" :idValue="ele.id" :key="ele.id" @from="fromFun" @to="toFun">{{ ele.symbol }}</Tile>
+
+<!--
+  <Tile @from="fromFun" @to="toFun">♗</Tile>
+  <Tile @from="fromFun" @to="toFun">♟︎</Tile>
+  <div class="tile">♔</-div>
+  <div class="tile">♗</div>
+  <div class="tile">♟︎</div>
+-->
 </template>
 <style scoped>
 
