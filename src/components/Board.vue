aac152ab9eac7c98e7c64429f228677b5770e829
Simon Gschnell <simongschnell@gmail.com>
2023-09-04 17:22:27 +0200

    colors the king if he was checked and resets the board if a player was checkmated

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -11,6 +11,7 @@ onBeforeMount(async () => {
 
 const from = ref("");
 const to = ref("");
+const check = ref("");
 
 function fromFun(id) {
   console.log(id);
@@ -23,24 +24,45 @@ function toFun(id) {
 
 async function getBoard() {
   const url = "http://127.0.0.1:8080/board";
+  const url_check = "http://127.0.0.1:8080/board/check";
   try {
+    //get board data
     let res = await fetch(url);
     let json = await res.json();
     console.log(json);
     board.value = json;
+
+    //get check data
+    let check_res = await fetch(url_check);
+    let check_json = await check_res.json();
+    console.log(check_json);
+    if (check_json.pos){
+      check.value = check_json.pos.file + check_json.pos.rank.toString();
+    }else{
+      check.value = "";
+    }
+    
   } catch (err) {
     console.log(err);
   }
 }
 
 async function updateBoard(from, to) {
-  const url = `http://127.0.0.1:8080/move/${from}/${to}`;
+  const url_move = `http://127.0.0.1:8080/move/${from}/${to}`;
+  const url_checkmate = `http://127.0.0.1:8080/board/checkmate`;
   try {
-    let res = await fetch(url);
+    let res = await fetch(url_move);
     let json = await res.json();
     console.log(json);
-    if (json == "success") {
-      getBoard();
+
+    let res_checkmate = await fetch(url_checkmate);
+    let json_checkmate = await res_checkmate.json();
+    console.log(json_checkmate);
+
+    if (json_checkmate.is_checkmate){
+      resetBoard()
+    }else{
+      getBoard()
     }
   } catch (err) {
     console.error(err);
@@ -48,7 +70,7 @@ async function updateBoard(from, to) {
 }
 
 async function resetBoard(){
-  const url = "http://127.0.0.1:8080/reset";
+  const url = "http://127.0.0.1:8080/board/reset";
   try {
     let res = await fetch(url);
     let json = await res.json();
@@ -91,11 +113,13 @@ async function resetBoard(){
       :piece_color="ele.piece_color"
       :idValue="ele.col + ele.row"
       :key="ele.col + ele.row"
+      :style="ele.col+ele.row ==check? 'background-color:red':''"
       @from="fromFun"
       @to="toFun"
-      >{{ ele.symbol }}</Tile
-    >
+      >{{ele.symbol }}</Tile>
+      
   </div>
+  <p style="color: red;">{{ check?"CHECK":"" }}</p>
 </template>
 <style scoped>
 .btn {
